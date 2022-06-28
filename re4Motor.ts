abcf4a30a9ac5f3d7c48e4df3aeb5a6b4f5039ca
Daniela Riedl <makkelaig@gmail.com>
2022-06-28 11:05:12 +0200

    put velocity number check in setter and make set and move functions private

--- re4Motor.ts
+++ re4Motor.ts
@@ -15,15 +15,15 @@ enum R4eTurn {
     Backward = 0x2,
 }
 
-let PWMA = AnalogPin.P8;
-let AIN1 = DigitalPin.P13;
-let AIN2 = DigitalPin.P12;
-let PWMB = AnalogPin.P16;
-let BIN1 = DigitalPin.P14;
-let BIN2 = DigitalPin.P15;
-let speedSetting = 10;
-
 namespace r4eMotor {
+
+    let PWMA = AnalogPin.P8;
+    let AIN1 = DigitalPin.P13;
+    let AIN2 = DigitalPin.P12;
+    let PWMB = AnalogPin.P16;
+    let BIN1 = DigitalPin.P14;
+    let BIN2 = DigitalPin.P15;
+    let speedSetting = 10;
 
     export function motorStop(m: R4eMotor): void {
         if (m == R4eMotor.Right)
@@ -33,7 +33,8 @@ namespace r4eMotor {
     }
 
     export function motorRun(m: R4eMotor, index: R4eTurn, speed: number): void {
-        speed = speed * 64 - 1; // map 0 to 1023
+        setSpeed(speed);
+        speed = r4eSpeed() * 64 - 1; // map 0 to 1023
         if (m == R4eMotor.Right) {
             pins.analogWritePin(PWMA, speed)
             if (index == R4eTurn.Forward) {
@@ -55,15 +56,17 @@ namespace r4eMotor {
         }
     }
 
-    export function setSpeed(newSpeed: number) {
+    function setSpeed(newSpeed: number) {
+        newSpeed = (newSpeed < 0) ? 0 : newSpeed;
+        newSpeed = (newSpeed > 16) ? 16 : newSpeed;
         speedSetting = newSpeed;
     }
     
-    export function speed() {
+    export function r4eSpeed() {
         return speedSetting;
     }
 
-    export function driveForward(seconds: number, speed: number) {
+    function driveForward(seconds: number, speed: number) :void {
         motorRun(R4eMotor.Right, R4eTurn.Forward, speed)
         motorRun(R4eMotor.Left, R4eTurn.Forward, speed)
         basic.pause(seconds * 1000)
@@ -71,7 +74,7 @@ namespace r4eMotor {
         motorStop(R4eMotor.Left)
     }
 
-    export function driveBackward(seconds: number, speed: number) {
+    function driveBackward(seconds: number, speed: number) :void {
         motorRun(R4eMotor.Right, R4eTurn.Backward, speed)
         motorRun(R4eMotor.Left, R4eTurn.Backward, speed)
         basic.pause(seconds * 1000)
@@ -79,7 +82,7 @@ namespace r4eMotor {
         motorStop(R4eMotor.Left)
     }
 
-    export function driveLeft(seconds: number, speed: number) {
+    function driveLeft(seconds: number, speed: number) :void {
         motorRun(R4eMotor.Right, R4eTurn.Forward, speed)
         motorRun(R4eMotor.Left, R4eTurn.Backward, speed)
         basic.pause(seconds * 1000)
@@ -87,12 +90,26 @@ namespace r4eMotor {
         motorStop(R4eMotor.Left)
     }
 
-    export function driveRight(seconds: number, speed: number) {
+    function driveRight(seconds: number, speed: number) :void {
         motorRun(R4eMotor.Right, R4eTurn.Backward, speed)
         motorRun(R4eMotor.Left, R4eTurn.Forward, speed)
         basic.pause(seconds * 1000)
         motorStop(R4eMotor.Right)
         motorStop(R4eMotor.Left)
+    }
+
+    export function moveInDirection(direction: R4eMotorDirection, seconds: number, speed: number): void {
+        setSpeed(speed);
+        speed = r4eMotor.r4eSpeed();
+        if (direction == R4eMotorDirection.forward) {
+            driveForward(seconds, speed)
+        } else if (direction == R4eMotorDirection.backward) {
+            driveBackward(seconds, speed)
+        } else if (direction == R4eMotorDirection.left) {
+            driveLeft(seconds, speed)
+        } else if (direction == R4eMotorDirection.right) {
+            driveRight(seconds, speed)
+        }
     }
 
 }
